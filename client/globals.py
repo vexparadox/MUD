1a9ddd580592cced31740f2099f0ece529c7e346
William Meaton <will.meaton@gmail.com>
2017-04-26 18:57:24 +0100

    added my url

--- client/globals.py
+++ client/globals.py
@@ -1,5 +1,5 @@
 userToken = "" # Stores the userToken given by the server after the user logs in
 loggedIn = False # If the user is logged in setting this to true abitrarily wont make things work
 running = True # If the program is running, set to False to stop
-baseURL = "http://localhost:5000" # The base url of the server
+baseURL = "http://willmeaton.com:5000" # The base url of the server
 worldmap = [] # This will be filled in later
