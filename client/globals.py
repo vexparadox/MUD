a4c66809143e5447335a5dfee77dca2a81bccb5f
William Meaton <will.meaton@gmail.com>
2017-04-27 14:09:14 +0100

    url for testing

--- client/globals.py
+++ client/globals.py
@@ -1,7 +1,7 @@
 userToken = "" # Stores the userToken given by the server after the user logs in
 loggedIn = False # If the user is logged in setting this to true abitrarily wont make things work
 running = True # If the program is running, set to False to stop
-baseURL = "http://localhost:5000" # The base url of the server
+baseURL = "http://willmeaton.com:5000" # The base url of the server
 worldmap = [] # This will be filled in later
 worldheight = 0
 worldwidth = 0
